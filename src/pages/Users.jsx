b2d1fb6fad40a365c7366958b8e6a1fa3035b67e
Altaf Hussain <76874013+iamaltaf786@users.noreply.github.com>
2025-04-24 13:51:23 +0530

    team css modified

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -28,23 +28,24 @@ const Users = () => {
   };
 
   const TableHeader = () => (
-    <thead className='border-b border-gray-300'>
-      <tr className='text-black text-left'>
-        <th className='py-2'>Full Name</th>
-        <th className='py-2'>Title</th>
-        <th className='py-2'>Email</th>
-        <th className='py-2'>Role</th>
-        <th className='py-2'>Active</th>
+    <thead className="border-b border-gray-300">
+      <tr className="bg-[#8fbc8f] text-center ">
+        <th className="py-2 border-r border-gray-300">Full Name</th>
+        <th className="py-2 border-r border-gray-300">Title</th>
+        <th className="py-2 border-r border-gray-300">Email</th>
+        <th className="py-2 border-r border-gray-300">Role</th>
+        <th className="py-2 border-r border-gray-300">Active</th>
+        <th className="py-2">Actions</th>
       </tr>
     </thead>
   );
 
   const TableRow = ({ user }) => (
-    <tr className='border-b border-gray-200 text-gray-600 hover:bg-gray-400/10'>
-      <td className='p-2'>
-        <div className='flex items-center gap-3'>
-          <div className='w-9 h-9 rounded-full text-white flex items-center justify-center text-sm bg-blue-700'>
-            <span className='text-xs md:text-sm text-center'>
+    <tr className="border-b border-gray-200 text-gray-600 hover:bg-gray-400/10">
+      <td className="p-2 border-r border-gray-200">
+        <div className="flex items-center gap-3">
+          <div className="w-9 h-9 rounded-full text-white flex items-center justify-center text-sm bg-blue-700">
+            <span className="text-xs md:text-sm text-center">
               {getInitials(user.name)}
             </span>
           </div>
@@ -52,34 +53,36 @@ const Users = () => {
         </div>
       </td>
 
-      <td className='p-2'>{user.title}</td>
-      <td className='p-2'>{user.email || "user.emal.com"}</td>
-      <td className='p-2'>{user.role}</td>
+      <td className="p-2 border-r border-gray-200">{user.title}</td>
+      <td className="p-2 border-r border-gray-200">
+        {user.email || "user.emal.com"}
+      </td>
+      <td className="p-2 border-r border-gray-200">{user.role}</td>
 
-      <td>
+      <td className="p-2 border-r border-gray-200">
         <button
           // onClick={() => userStatusClick(user)}
           className={clsx(
-            "w-fit px-4 py-1 rounded-full",
-            user?.isActive ? "bg-blue-200" : "bg-yellow-100"
+            "w-fit px-4 py-1 rounded-full text-white",
+            user?.isActive ? "bg-emerald-600/80" : "bg-yellow-100"
           )}
         >
           {user?.isActive ? "Active" : "Disabled"}
         </button>
       </td>
 
-      <td className='p-2 flex gap-4 justify-end'>
+      <td className="p-2 flex gap-4 justify-end">
         <Button
-          className='text-blue-600 hover:text-blue-500 font-semibold sm:px-0'
-          label='Edit'
-          type='button'
+          className="text-blue-600 hover:text-blue-500 font-semibold sm:px-0"
+          label="Edit"
+          type="button"
           onClick={() => editClick(user)}
         />
 
         <Button
-          className='text-red-700 hover:text-red-500 font-semibold sm:px-0'
-          label='Delete'
-          type='button'
+          className="text-red-700 hover:text-red-500 font-semibold sm:px-0"
+          label="Delete"
+          type="button"
           onClick={() => deleteClick(user?._id)}
         />
       </td>
@@ -88,20 +91,20 @@ const Users = () => {
 
   return (
     <>
-      <div className='w-full md:px-1 px-0 mb-6'>
-        <div className='flex items-center justify-between mb-8'>
-          <Title title='  Team Members' />
+      <div className="w-full md:px-1 px-0 mb-6">
+        <div className="flex items-center justify-between mb-8">
+          <Title title="  Team Members" />
           <Button
-            label='Add New User'
-            icon={<IoMdAdd className='text-lg' />}
-            className='flex flex-row-reverse gap-1 items-center bg-blue-600 text-white rounded-md 2xl:py-2.5'
+            label="Add New User"
+            icon={<IoMdAdd className="text-lg" />}
+            className="flex flex-row-reverse gap-1 items-center bg-blue-600 text-white rounded-md 2xl:py-2.5"
             onClick={() => setOpen(true)}
           />
         </div>
 
-        <div className='bg-white px-2 md:px-4 py-4 shadow-md rounded'>
-          <div className='overflow-x-auto'>
-            <table className='w-full mb-5'>
+        <div className="bg-[#b0d2de] px-2 md:px-4 py-4 shadow-md rounded">
+          <div className="overflow-x-auto">
+            <table className="w-full mb-5">
               <TableHeader />
               <tbody>
                 {summary.users?.map((user, index) => (
