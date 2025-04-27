caccbf575547b8a10fcceeb65e5038eb97e21835
1100Ram0011 <rambachanyadav558@gmail.com>
2025-04-27 15:13:36 +0530

    changes function error slove

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -45,6 +45,7 @@ export const registerUser = async (req, res) => {
 export const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
+      console.log(email, password)
 
     const user = await User.findOne({ email });
 
